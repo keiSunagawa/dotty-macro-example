215dc5437bdbbbc89477af25bacc5c8dbd8cc4b9
keiSunagawa <kei.sunagawa@mvrck.co.jp>
2019-07-22 17:04:31 +0900

    うごかない

--- macro-exammple/macros/src/main/scala/macros.scala
+++ macro-exammple/macros/src/main/scala/macros.scala
@@ -24,50 +24,58 @@ object DoSyntax {
     lf: Liftable[F[A]]
   ): Expr[F[A]] = {
     implicit val toolbox: scala.quoted.Toolbox = scala.quoted.Toolbox.make(getClass.getClassLoader)
-    var env = Map.empty[String, Any]
 
-    println(xs.show)
-
-    def matchRHS(rhs: Expr[RHS[A, F]]): Expr[F[A]] = rhs match {
-      case '{ Value($a: F[A]) } =>
+    def matchRHS[B: Type](rhs: Expr[RHS[B, F]])(implicit env: Map[Expr[String], Expr[F[Any]]]): Expr[F[B]] = rhs match {
+      case '{ Value($a: F[B]) } =>
         a
       case '{ Get($s, $f) } =>
-        // matchRHS('{
-        // val a = env($s)
-        // ${f}.asInstanceOf[Any => RHS[A, F]].apply(a)
-        // })
+        val a = env(s)
+        matchRHS('{
+          ${f}.asInstanceOf[Any => RHS[B, F]].apply($a)
+        })
         ???
     }
-    xs match {
-      case '{ (Bind($a, $f: RHS[A, F]): Bind[A, F]) } => f match {
-        case '{ Value($a: F[A]) } =>
-          a
+    def go[B: Type](ex: Expr[Syntax[B, F]])(implicit env: Map[Expr[String], Expr[F[Any]]]): Expr[F[B]] = {
+      ex match {
+        case '{ (Bind($a, $f: RHS[B, F]): Bind[B, F]) } =>
+          println("== bind match ==")
+          matchRHS(f)
+        case '{ Bind($a, $f: Value[B, F]) } =>
+          println("== bind match 2==")
+          matchRHS(f)
+        case '{ Do($a: Bind[_, F], $b: Bind[_, F])} =>
+          println("== do match ==")
+          go[B](b.asInstanceOf[Expr[Syntax[B, F]]])
         case _ =>
-          println("===unmatch===")
-          println(f)
+          println("== unmatch ==")
+//          println(ex.show)
           ???
       }
-      case '{ Bind($a, $f: Value[A, F]) } => f match {
-        case '{ Value($a: F[A]) } =>
-          a
-        case _ =>
-          println("===unmatch===")
-          println(f)
-          ???
-      }
-      case '{ $a } =>
-        ???
     }
+    implicit val e: Map[Expr[String], Expr[F[Any]]] = Map.empty
+    go(xs)
   }
 }
 
 object DSL {
   // TODO enum
-  sealed trait Exp[A, F[_]]
-  sealed trait Syntax[A, F[_]] extends Exp[A, F]
+  sealed trait Exp[+A, F[_]]
+  sealed trait Syntax[+A, F[_]] extends Exp[A, F]
   sealed trait RHS[A, F[_]] extends Exp[A, F]
   case class Bind[A, F[_]](symbol: String, f: RHS[A, F]) extends Syntax[A, F]
-  case class Do[A, B, F[_]](l: Bind[A, F], r: Syntax[B, F]) extends Syntax[B, F]
+  abstract class Do[A, F[_]] extends Syntax[A, F] {
+    type B
+    val l: Bind[B, F]
+    val r: Syntax[A, F]
+  }
+  object Do {
+    def apply[A, C, F[_]](lo: Bind[C, F], ro: Bind[A, F]): Do[A, F] = new Do {
+      type B = C
+      val l = lo
+      val r = ro
+    }
+    def unapply[A, F[_]](d: Do[A, F]): Option[(Bind[_, F], Syntax[A, F])] = Some(d.l -> d.r)
+  }
 
   case class Value[A, F[_]](v: F[A]) extends RHS[A, F]
   case class Get[A, B, F[_]](symbol: String, f: B => RHS[A, F]) extends RHS[A, F]
--- macro-exammple/src/main/scala/Main.scala
+++ macro-exammple/src/main/scala/Main.scala
@@ -3,8 +3,14 @@ object Main extends App {
   import delegate Lif._
 
   val d = DoSyntax.dos{
-     "a" </ Option(2)
+    // Do("a" </ Option(2), "b" </ Option(3))
+    Do[Int, Int, Option](
+      Bind("a", Value(Option(2))),
+      Bind("b", Value(Option(3)))
+    )
   }
+//    val d = DoSyntax.dos{ "a" </ Option(2) }
+
   println(d)
 
   def hoge(): Option[Int] = None
